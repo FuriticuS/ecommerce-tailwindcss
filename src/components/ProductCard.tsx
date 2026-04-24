5216ee11086ce6fa7613065de9600e5d15528cb5
Fur1ouS <Fur1ouS@ya.ru>
2026-04-25 02:25:10 +0300

    add component Product Card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,19 +7,17 @@ export default function ProductCard({product}: { product: Product }) {
       to={`/product/${product.id}`}
       className="relative flex flex-col items-center justify-center w-fit group"
     >
-  <span
-    className="absolute font-semibold text-white text-center text-xl z-10 group-hover:text-red-500 group-hover:animate-wiggle transition duration-1300">
-    {product.name}
-    <br/>
-    {product.price}$
-  </span>
+      <span className="absolute font-semibold text-white text-center text-xl z-10 group-hover:text-red-500 group-hover:animate-wiggle transition duration-1300">
+        {product.name}
+        <br/>
+        {product.price}$
+      </span>
       <img
         className="rounded-md w-5xl"
         src={product.img}
         alt={product.name}
       />
-      <div
-        className="absolute inset-0 bg-gradient-to-b from-avocado-600 via-black to-avocado-500 opacity-20 rounded-md"/>
+      <div className="absolute inset-0 bg-gradient-to-b from-avocado-600 via-black to-avocado-500 opacity-20 rounded-md"/>
     </Link>
   );
 }
